a99ad547b87f73a54e5eaddfa5dd4ddc5d863682
billybox1926-jpg <billybox1926@gmail.com>
2026-05-24 01:12:50 -0700

    fix: rewrite terraform-plan-parser formula as source-build v0.1.3
    - Bump version from v0.1.3
    - Switch from binary matrix to source tarball build
    - Remove placeholder sha256 values
    - Remove on_windows branch (Windows is non-Homebrew path per policy)
    - Add depends_on rust => :build
    - Update CI to audit/install/test terraform-plan-parser

--- Formula/terraform-plan-parser.rb
+++ Formula/terraform-plan-parser.rb
@@ -1,39 +1,14 @@
 class TerraformPlanParser < Formula
   desc "Lightweight CLI that turns Terraform plan JSON into clean summaries"
   homepage "https://github.com/billybox1926-jpg/terraform-plan-parser"
-  version "0.1.0"
+  url "https://github.com/billybox1926-jpg/terraform-plan-parser/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "cd752a798d036b543bb091ec6a70c3e9c877356846d884ee39283c1537a71a62"
+  license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/billybox1926-jpg/terraform-plan-parser/releases/download/v0.1.0/terraform_plan_parser-macos-arm64.tar.gz"
-      sha256 "UPDATE_WITH_ACTUAL_SHA256"
-    else
-      url "https://github.com/billybox1926-jpg/terraform-plan-parser/releases/download/v0.1.0/terraform_plan_parser-macos-x64.tar.gz"
-      sha256 "e4e429677d41d69db1b644a5cf5fc2238aa98305c7b65ef498f2ebcac8ed6376"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/billybox1926-jpg/terraform-plan-parser/releases/download/v0.1.0/terraform_plan_parser-linux-arm64.tar.gz"
-      sha256 "UPDATE_WITH_ACTUAL_SHA256"
-    else
-      url "https://github.com/billybox1926-jpg/terraform-plan-parser/releases/download/v0.1.0/terraform_plan_parser-linux-x64.tar.gz"
-      sha256 "904554101fd540fdecb5d41e37a8a49b61fad62b5b93675578d88af2293e30d0"
-    end
-  end
-
-  on_windows do
-    url "https://github.com/billybox1926-jpg/terraform-plan-parser/releases/download/v0.1.0/terraform_plan_parser-windows-x64.zip"
-    sha256 "86b71ec7db3c6464cdd6f2ee2d9942c81dfc7237da2b6fa7e831b387e3a159da"
-  end
+  depends_on "rust" => :build
 
   def install
-    if OS.windows?
-      bin.install "terraform_plan_parser.exe" => "terraform_plan_parser"
-    else
-      bin.install "terraform_plan_parser"
-    end
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
